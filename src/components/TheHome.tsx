d2533bf20bc5d31fc44fb18c0a4ec815c64998e7
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-05-16 11:40:21 -0400

    Melhorias na home, tirando coisas desnecessárias

--- src/components/TheHome.tsx
+++ src/components/TheHome.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export function TheHome(){
   return(
-    <div className="text-branco mx-36 bg-zinc-main sm:py-11 sm:px-28 px-4 flex flex-col sm:flex-row items-center justify-between">
-      <div className="flex-col mt-28 sm:mr-16 w-3/6">
+    <div className="text-branco sm:mx-36 bg-zinc-main sm:py-11 flex flex-col sm:flex-row items-center justify-between">
+      <div className="flex-col mt-10 w-3/6">
         <p className="text-3xl">Olá, eu sou</p>
-        <h1 className="sm:text-7xl text-5xl font-semibold my-5">Juan Gomes</h1>
+        <h1 className="sm:text-6xl text-5xl font-semibold my-5">Juan Gomes</h1>
         <h2 className="text-3xl mb-10 font-semibold text-blue-2">Desenvolvedor</h2>
-        <p className="text-justify text-zinc-500">Olá, meu nome é Juan Gomes, sou estudante de 
+        <p className="text-justify text-sm text-zinc-500">Olá, meu nome é Juan Gomes, sou estudante de 
         Engenharia da Computação e um jovem entusiasta da tecnologia. Com experiência em suporte e 
         desenvolvimento de software, estou sempre procurando novos desafios para aprimorar minhas 
         habilidades e evoluir cada vez mais como DEV. Ao longo de minha carreira, adquiri habilidades 
@@ -17,7 +17,7 @@ export function TheHome(){
       </div>
 
       <div className="w-3/6">
-        <Image src="/Dev animation.gif" width={700} height={700} alt="Imagem de computador"/>
+        <Image src="/Dev animation.gif" width={800} height={700} alt="Imagem de um desenvolvedor programando"/>
       </div>
     </div>
   )
