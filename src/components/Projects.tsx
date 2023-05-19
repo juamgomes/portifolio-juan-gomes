b58f3f122c3e18002269ad076979b381856c0438
Juan Gomes <89032763+juamgomes@users.noreply.github.com>
2023-05-19 13:39:19 -0400

    Coloquei um MAP nos projetos e criei um banco local com todas informações dos projetos, pra facilita quando for adicionar o projeto novo. E pra deixar o código mais limpo e de fácil entendimento.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,98 +1,76 @@
 import { IoLogoGithub, IoMdLink } from "react-icons/io"
 import Image from "next/image"
+
+
+
 export function Projects(){
-  return(
-    <div> 
-      <h1 className="text-branco text-center mb-10 font-semibold text-xl">Projetos</h1>
-      <div className="grid grid-cols-1 gap-10 place-items-center">
-        <div className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
-          <Image src="/Spotify-img.JPG" width={488} height={288} className="rounded-xl" alt="Print da tela do Spotify"/>
+
+  const  myProjectsData = [
+    {image: '/Spotify-img.JPG', 
+    name: 'Interface do Spotify', 
+    tecnologias: 'Next - Tailwind - React', 
+    description: 'Clone da interface do spotify.',
+    linkGit: 'https://github.com/juamgomes/spotify-interface',
+    linkProject: 'https://spotify-interface-gamma.vercel.app/'},
+
+    {image: '/Sistema-de-controle-financeiro-img.JPG', 
+    name: 'Controle financeiro', 
+    tecnologias: 'React - JS - StylesComponents', 
+    description: 'Sistema de controle financeiro que registra as entradas e saídas de um valor inicial, subtrai e mostra o total. Para fazer um planejamento financeiro.',
+    linkGit: 'https://github.com/juamgomes/controle-financeiro',
+    linkProject: 'https://controle-financeiro-eosin.vercel.app/'},
+    
+    {image: '/Lista-de-tarefas-img.JPG', 
+    name: 'Lista de tarefas', 
+    tecnologias: 'React - JS - Tailwindcss', 
+    description: 'Lista de tarefas, para organizar e planejar as tarefas do dia a dia.',
+    linkGit: 'https://github.com/juamgomes/Lista-de-tarefas',
+    linkProject: 'https://lista-de-tarefas-mu-dusky.vercel.app/'},
+
+    {image: '/Relogio-digital-img.jpg', 
+    name: 'Relógio digital', 
+    tecnologias: 'HTML - CSS - JS', 
+    description: 'Rélogio digital simples.',
+    linkGit: 'https://github.com/juamgomes/relogio-digital',
+    linkProject: 'https://juamgomes.github.io/relogio-digital/'},
+
+    {image: '/Conversor-de-temperatura-img.JPG', 
+    name: 'Conversor de temperatura', 
+    tecnologias: 'HTML - CSS - JS', 
+    description: 'Conversor de temperaturas conversão entre °C e °F, conversão entre °F e °C, conversão entre °F e K, conversão entre K e °F conversão entre °C e K e conversão entre K e °C.',
+    linkGit: 'https://github.com/juamgomes/Conversor-de-temperaturas',
+    linkProject: 'https://juamgomes.github.io/Conversor-de-temperaturas/'}
+  ];
+
+    const ProjectsCards = () => {
+    return myProjectsData.map((pro) => (
+      <div key={pro.name} className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
+          <Image src={pro.image} width={488} height={288} className="rounded-xl" alt="Print da tela do Spotify"/>
           <div className="flex flex-col gap-4">
-            <h1 className="text-blue-2 text-4xl font-semibold">Interface do Spotify</h1>
-            <span className="text-zinc-600 font-semibold">Next - Tailwind - React</span>
-            <p className="sm:flex text-zinc-500 hidden">Clone da interface do spotify.</p>
+            <h1 className="text-blue-2 text-4xl font-semibold">{pro.name}</h1>
+            <span className="text-zinc-600 font-semibold">{pro.tecnologias}</span>
+            <p className="sm:flex text-zinc-500 hidden">{pro.description}</p>
             <div className="flex gap-4">
-                <a href="https://github.com/juamgomes/spotify-interface" target="_blank">
+                <a href={pro.linkGit} target="_blank">
                   <IoLogoGithub size={30} className="text-branco mt-10"/>
                 </a>
 
-                <a href="https://spotify-interface-gamma.vercel.app/" target="_blank">
+                <a href={pro.linkProject} target="_blank">
                   <IoMdLink size={30} className="text-branco mt-10"/>
                 </a>
               
             </div>
           </div>
         </div>
+    ));
+  }
+  return(
+    <div> 
+      <h1 className="text-branco text-center mb-10 font-semibold text-xl">Projetos</h1>
+      <div className="grid grid-cols-1 gap-10 place-items-center">
 
-        <div className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
-        <Image src="/Sistema-de-controle-financeiro-img.JPG" width={488} height={288} className="rounded-xl" alt="Print da tela do sistema de controle financeiro"/>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-blue-2 text-4xl font-semibold">Controle financeiro</h1>
-            <span className="text-zinc-600 font-semibold">React - JS - StylesComponents</span>
-            <p className="sm:flex text-zinc-500 hidden">Sistema de controle financeiro que registra as entradas e saídas de um valor inicial, subtrai e mostra o total. Para fazer um planejamento financeiro.</p>
-            <div className="flex gap-4">
-              <a href="https://github.com/juamgomes/controle-financeiro" target="_blank">
-                <IoLogoGithub size={30} className="text-branco mt-10"/>
-              </a>
-              <a href="https://controle-financeiro-eosin.vercel.app/" target="_blank">
-                <IoMdLink size={30} className="text-branco mt-10"/>
-              </a>
-            </div>
-          </div>
-        </div>
-
-
-        <div className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
-        <Image src="/Lista-de-tarefas-img.JPG" width={488} height={288} className="rounded-xl" alt="Print da tela da lista de tarefas"/>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-blue-2 text-4xl font-semibold">Lista de tarefas</h1>
-            <span className="text-zinc-600 font-semibold">React - JS - Tailwindcss</span>
-            <p className="text-zinc-500 sm:flex hidden">Lista de tarefas, para organizar e planejar as taerefas do dia a dia.</p>
-            <div className="flex gap-4">
-              <a href="https://github.com/juamgomes/Lista-de-tarefas" target="_blank">
-                <IoLogoGithub size={30} className="text-branco mt-10"/>
-              </a>
-              <a href="https://lista-de-tarefas-mu-dusky.vercel.app/" target="_blank">
-                <IoMdLink size={30} className="text-branco mt-10"/>
-              </a>
-            </div>
-          </div>
-        </div>
-        
-          <div className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
-          <Image src="/Relogio-digital-img.jpg" width={488} height={288} className="rounded-xl" alt="Relógio digital"/>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-blue-2 text-4xl font-semibold">Relógio digital</h1>
-            <span className="text-zinc-600 font-semibold">HTML - CSS - JS</span>
-            <p className="sm:flex text-zinc-500 hidden">Rélogio digital simples.</p>
-            <div className="flex gap-4">
-              <a href="https://github.com/juamgomes/relogio-digital" target="_blank">
-                <IoLogoGithub size={30} className="text-branco mt-10"/>
-              </a>
-              <a href="https://juamgomes.github.io/relogio-digital/" target="_blank">
-                <IoMdLink size={30} className="text-branco mt-10"/>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col sm:flex-row sm:gap-10 gap-6 p-4 w-3/4 h-auto rounded-xl bg-zinc-800">
-          <Image src="/Conversor-de-temperatura-img.JPG" width={488} height={288} className="rounded-xl" alt="Conversor de temperatura"/>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-blue-2 text-4xl font-semibold">Conversor de temperatura</h1>
-            <span className="text-zinc-600 font-semibold">HTML - CSS - JS</span>
-            <p className="sm:flex text-zinc-500 hidden">Conversor de temperaturas conversão entre °C e °F, conversão entre °F e °C, conversão entre °F e K, conversão entre K e °F conversão entre °C e K e conversão entre K e °C.</p>
-            <div className="flex gap-4">
-              <a href="https://github.com/juamgomes/Conversor-de-temperaturas" target="_blank">
-                <IoLogoGithub size={30} className="text-branco mt-10"/>
-              </a>
-              <a href="https://juamgomes.github.io/Conversor-de-temperaturas/" target="_blank">
-                <IoMdLink size={30} className="text-branco mt-10"/>
-              </a>
-            </div>
-          </div>
-        </div>
-      
+        {ProjectsCards()}
+    
       </div>
     </div>
   )
